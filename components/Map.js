0fb4706e0962f54324c83e99050e70bca8126db1
Benedict <benedictryan80@gmail.com>
2023-08-04 17:24:55 +0700

    remove dead codes and comments

--- components/Map.js
+++ components/Map.js
@@ -3,14 +3,10 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import { locations } from '@/locations';
-import { getRoute } from '@/utils/generateRoute';
 import {
 	generateNewMarker,
 	generateGeocoderMarker
 } from '@/utils/generateMarkers';
-
-// const mapStyle = 'mapbox://styles/benryan/clkuv54ck000u01po9b59cvgr';
 
 const dark = 'mapbox://styles/benryan/clkuv54ck000u01po9b59cvgr';
 
@@ -37,84 +33,6 @@ const Map = ({ mapboxToken }) => {
 	// const [zoom, setZoom] = useState(9);
 
 	const mapNode = useRef(null);
-
-	// const generateGeocoderMarker = (result, mapboxMap, geocoder) => {
-	// 	const { text, center } = result;
-
-	// 	const [lng, lat] = center;
-
-	// 	// ? understand this
-	// 	if (geoMarker) {
-	// 		geoMarker.remove();
-	// 	}
-
-	// 	const markerElement = document.createElement('div');
-	// 	markerElement.className = 'custom-marker text-[50px] text-center';
-	// 	markerElement.innerHTML = `🇺🇸`;
-
-	// 	const textElement = document.createElement('div');
-	// 	textElement.innerHTML = text;
-	// 	textElement.className = `text-center ${
-	// 		mapStyle === light ? 'text-black' : 'text-white'
-	// 	} text-lg font-medium `;
-
-	// 	// Create a button element
-	// 	const buttonElement = document.createElement('button');
-	// 	buttonElement.textContent = 'Show route';
-	// 	buttonElement.className =
-	// 		'bg-blue-500 text-white px-3 py-1 rounded mx-auto';
-
-	// 	// Center the button horizontally using flexbox
-	// 	const buttonContainer = document.createElement('div');
-	// 	buttonContainer.className = 'flex justify-center';
-	// 	buttonContainer.appendChild(buttonElement);
-
-	// 	buttonElement.addEventListener('click', () => {
-	// 		// Perform an action when the button is clicked
-	// 		// console.log(buttonElement.textContent);
-	// 		// toggleButtonText();
-	// 		getRoute(mapboxToken, mapboxMap, lng, lat, mapStyle, light);
-	// 	});
-
-	// 	const markerContainer = document.createElement('div');
-	// 	markerContainer.appendChild(markerElement);
-	// 	markerContainer.appendChild(textElement);
-	// 	markerContainer.appendChild(buttonContainer);
-
-	// 	// geoMarker = new mapboxgl.Marker(markerContainer)
-	// 	geoMarker = new mapboxgl.Marker(markerContainer)
-	// 		.setLngLat(center)
-	// 		.setPopup(
-	// 			new mapboxgl.Popup({
-	// 				closeButton: false,
-	// 				anchor: 'left'
-	// 			}).setHTML(` <div class="bg-white  p-2">
-
-	//             <p class="text-black text-lg font-medium">You are in: ${text}</p>
-
-	//               <p class="text-black text-[14px]">Coordinate: ${lng}, ${lat}</p>
-
-	//            </div>`)
-	// 		)
-	// 		.addTo(mapboxMap);
-
-	// 	// Listen to the 'clear' event of the geocoder
-	// 	geocoder.on('clear', () => {
-	// 		// Remove the custom marker from the map
-	// 		if (geoMarker) {
-	// 			geoMarker.remove();
-	// 			// mapboxMap.removeLayer('route');
-	// 			// mapboxMap.removeSource('route');
-	// 			// console.log(mapboxMap.getSource('route'));
-	// 			if (mapboxMap.getLayer('route')) {
-	// 				mapboxMap.removeLayer('route');
-	// 			}
-	// 			if (mapboxMap.getSource('route')) {
-	// 				mapboxMap.removeSource('route');
-	// 			}
-	// 		}
-	// 	});
-	// };
 
 	useEffect(() => {
 		const node = mapNode.current;
